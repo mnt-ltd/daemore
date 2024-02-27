d57afb9f5e67a8b5a9324c27fe510b22434a6de1
truthhun <1272881215@qq.com>
2024-02-27 13:34:24 +0800

    Add error handling and remove unused dependencies

--- daemore.go
+++ daemore.go
@@ -1,16 +1,15 @@
 package daemore
 
 import (
+	"errors"
 	"os"
 
 	"github.com/kardianos/service"
-	"go.uber.org/zap"
 )
 
 type Daemon struct {
 	config   *service.Config
 	errs     chan error
-	logger   *zap.Logger
 	callback func()
 	Service  service.Service
 }
@@ -19,34 +18,33 @@ type DaemonOption struct {
 	Name        string
 	DisplayName string
 	Description string
-	Logger      *zap.Logger
-	RunCallback func()
+	Callback    func()
 }
 
 func NewDaemon(opt DaemonOption, username ...string) (d *Daemon, err error) {
 	config := &service.Config{
-		Name:        opt.Name,        //服务显示名称
-		DisplayName: opt.DisplayName, //服务名称
-		Description: opt.Description, //服务描述
+		Name:        opt.Name,
+		DisplayName: opt.DisplayName,
+		Description: opt.Description,
 	}
 	if len(username) > 0 {
 		config.UserName = username[0]
 	}
 
-	if opt.Logger == nil {
-		opt.Logger, _ = zap.NewDevelopment()
-	}
-
 	d = &Daemon{
 		config:   config,
 		errs:     make(chan error, 100),
-		logger:   opt.Logger.Named("daemore"),
-		callback: opt.RunCallback,
+		callback: opt.Callback,
+	}
+
+	if opt.Callback == nil {
+		err = errors.New("callback is required")
+		return
 	}
 
 	d.Service, err = service.New(d, d.config)
 	if err != nil {
-		d.logger.Fatal("new", zap.Error(err))
+		return nil, err
 	}
 	return
 }
@@ -62,9 +60,8 @@ func (d *Daemon) Start(s service.Service) error {
 
 func (d *Daemon) Run() {
 	if d.callback == nil {
-		d.logger.Fatal("run", zap.String("callback", "callback is nil"))
+		return
 	}
-	d.logger.Info("run...")
 	d.callback()
 }
 
@@ -75,46 +72,52 @@ func (d *Daemon) Stop(s service.Service) error {
 	return nil
 }
 
-// RunAsService 运行为服务
-func (d *Daemon) Install(args ...string) {
-	d.logger.Debug("install", zap.Any("args", args))
+func (d *Daemon) ServiceInstall(args ...string) (err error) {
 	d.config.Arguments = args
 	s, err := service.New(d, d.config)
 	if err != nil {
-		d.logger.Fatal("install", zap.Error(err))
+		return err
 	}
-	err = s.Install()
-	if err != nil {
-		d.logger.Fatal("install", zap.Error(err))
-	}
-	d.logger.Info("install success")
-	os.Exit(0)
+	return s.Install()
 }
 
-// RunAsService 运行为服务
-func (d *Daemon) Uninstall() {
+func (d *Daemon) ServiceUninstall() (err error) {
 	s, err := service.New(d, d.config)
 	if err != nil {
-		d.logger.Fatal("uninstall", zap.Error(err))
+		return err
 	}
-	err = s.Uninstall()
-	if err != nil {
-		d.logger.Fatal("uninstall", zap.Error(err))
-	}
-	d.logger.Info("uninstall success")
-	os.Exit(0)
+	return s.Uninstall()
 }
 
-// RunAsService 运行为服务
-func (d *Daemon) Restart() {
+func (d *Daemon) ServiceRestart() (err error) {
 	s, err := service.New(d, d.config)
 	if err != nil {
-		d.logger.Fatal("restart", zap.Error(err))
+		return err
 	}
-	err = s.Restart()
+	s.Stop()
+	return s.Start()
+}
+
+func (d *Daemon) ServiceStop() (err error) {
+	s, err := service.New(d, d.config)
 	if err != nil {
-		d.logger.Fatal("restart", zap.Error(err))
+		return err
 	}
-	d.logger.Info("restart success")
-	os.Exit(0)
+	return s.Stop()
+}
+
+func (d *Daemon) ServiceStart() (err error) {
+	s, err := service.New(d, d.config)
+	if err != nil {
+		return err
+	}
+	return s.Start()
+}
+
+func (d *Daemon) ServiceStatus() (status service.Status, err error) {
+	s, err := service.New(d, d.config)
+	if err != nil {
+		return status, err
+	}
+	return s.Status()
 }
--- go.mod
+++ go.mod
@@ -2,12 +2,6 @@ module github.com/mnt-ltd/daemore
 
 go 1.18
 
-require (
-	github.com/kardianos/service v1.2.2
-	go.uber.org/zap v1.26.0
-)
+require github.com/kardianos/service v1.2.2
 
-require (
-	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
-)
+require golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
